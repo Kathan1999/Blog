150aedf3323596492380d1cc8522d103a500fda9
Kathan Sheth <kathansheth@Kathans-MacBook-Pro.local>
2025-06-20 19:49:46 +0530

    templates has been added for a home page

--- Home/views.py
+++ Home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is my first home page")
+    return render(request, 'index.html')
