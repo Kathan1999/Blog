f87b0b6d2c8851b73e5931d2a861e5ee22d348ed
Kathan Sheth <kathansheth@Kathans-MacBook-Pro.local>
2025-06-26 23:54:55 +0530

    home introduction has been added

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, HttpResponse
 from Home.models import Contact
-from django.core.mail import send_mail
 # Create your views here.
 
 def index(request):
@@ -11,12 +10,4 @@ def index(request):
         message = request.POST.get('message')
         contact = Contact(name=name, subject=subject, email=email, message=message)
         contact.save()
-        message_body = f"Name: {name}\nEmail: {email}\n\nMessage:\n{message}"
-
-        send_mail(
-            subject,
-            message_body,
-            email,
-            ['shethkathan4@gmail.com']
-        )
     return render(request, 'index.html')
