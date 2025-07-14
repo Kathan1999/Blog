ec2110ef82dd1e2e09693dd6d5b8973bf0a1a051
Kathan Sheth <kathansheth@Kathans-MacBook-Pro.local>
2025-07-14 12:40:28 +0530

    some check done and if condition set out for username and password

--- Home/views.py
+++ Home/views.py
@@ -40,7 +40,18 @@ def handleSignup(request):
         pass2 = request.POST['pass2']
 
         #check for errorneous inputs
+        if len(username) > 10:
+            messages.error(request, "Username must be under 10 characters")
+            return redirect('home')
+        
+        if not username.isalnum():
+            messages.error(request, "Username should only contain letters and numbers")
+            return redirect('home')
 
+        if pass1 != pass2:
+            messages.error(request, 'password do not match')
+            return redirect('home')
+        
         #Create the User
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
